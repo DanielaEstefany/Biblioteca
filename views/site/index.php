108ac745443342c56802dfe6262410bd24fbc950
DanielaEstefany <estefanymunoz0@gmail.com>
2023-09-15 10:10:42 -0400

    se agrega el mensaje

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'Biblioteca CITD';
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
         <h1 class="display-4">BIBLIOTECA</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Haz logrado hacer el primer commit</p>
 
         <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
     </div>
